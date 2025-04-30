ff31b6636766daa9939ac9b6e2ef5e78a54e0ff7
Ardian Pradana <ardianysp18@gmail.com>
2025-05-01 01:35:20 +0700

    Edit project page

--- patterns/page-project.php
+++ patterns/page-project.php
@@ -14,18 +14,6 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>I focus on developing block plugins and Editor extensions for WordPress, and I've also built a few block themes along the way.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>You'll find a mix of my current projects here—some are actively being worked on, while others are older plugins that continue to be maintained. Many of these projects are used to power this site, and I hope they prove useful to you</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:heading -->
-<h2 class="wp-block-heading">Experimental</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
 <p>My WordPress projects consist of custom blocks, block themes, and Editor extensions. Many of them are experimental or created for my personal use, so they might not work seamlessly across all environments. You can explore more on GitHub.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
