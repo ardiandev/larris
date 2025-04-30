57f7fb7a4179b76b95fa1c308116c3b522eb324e
Ardian Pradana <ardianysp18@gmail.com>
2025-04-30 22:57:57 +0700

    Add blog page, contact page, projects page, and about page

---
+++ patterns/page-about.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: About Page
+ * Slug: larris/page-about
+ * Categories: banner
+ */
+?>
+
+<!-- wp:pattern {"slug":"larris/header"} /-->
+ 
+<!-- wp:group {"style":{"dimensions":{"minHeight":"60vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="min-height:60vh"><!-- wp:heading -->
+<h2 class="wp-block-heading">About</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":""} -->
+<p>Hi, I'm Ardian — a WordPress developer and plugin specialist based in Jakarta, Indonesia, where I live with my wonderful wife and daughter.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":""} -->
+<p>This website is my personal space for sharing everything related to web development, including my latest projects, handy code snippets, and tutorials focused on WordPress plugin development.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":""} -->
+<p>If you're interested in my plugins, be sure to check out the <strong>Projects</strong> page. Right now, I'm actively working on <strong>Block Styler</strong>, a plugin designed to enhance and extend the capabilities of WordPress core blocks.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":""} -->
+<p>Thanks for visiting!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:pattern {"slug":"larris/footer"} /-->
---
+++ patterns/page-contact.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Contact Page
+ * Slug: larris/page-contact
+ * Categories: banner
+ */
+?>
+
+<!-- wp:pattern {"slug":"larris/header"} /-->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"60vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="min-height:60vh"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"x-large","fontFamily":"poppins"} -->
+<h2 class="wp-block-heading has-poppins-font-family has-x-large-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700">Contact</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-bottom:0">Fill out the form below, and I’ll get back to you as soon as possible. If you’re looking for a more timely response, feel free to reach out on social.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- wp:pattern {"slug":"larris/footer"} /-->
