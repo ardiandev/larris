dd483914dde46c1c3efad6754e0a7981aae2419f
Ardian Pradana <ardianysp18@gmail.com>
2025-05-01 15:36:07 +0700

    Edit contact page template

--- patterns/page-contact.php
+++ patterns/page-contact.php
@@ -18,12 +18,4 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"larris/footer","name":"Footer"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">© 2025 Ardian Pradana. All rights reserved.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
-
-
-
 <!-- wp:pattern {"slug":"larris/footer"} /-->
