11ded97db96eded15088b83c7b59451ae637e2e9
Ardian Pradana <ardianysp18@gmail.com>
2025-05-01 09:44:39 +0700

    edit header

--- patterns/header.php
+++ patterns/header.php
@@ -8,9 +8,11 @@
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"20px","left":"10px","right":"10px","top":"20px"}}},"layout":{"type":"constrained","contentSize":""}} -->
 <div class="wp-block-group alignfull" style="padding-top:20px;padding-right:10px;padding-bottom:20px;padding-left:10px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large","fontFamily":"roboto"} /-->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"medium","fontFamily":"roboto"} /-->
 
-<!-- wp:navigation {"ref":4,"icon":"menu","className":"nav-container","style":{"spacing":{"blockGap":"10px"}},"fontSize":"medium","layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"icon":"menu","className":"nav-container","style":{"spacing":{"blockGap":"10px"}},"fontSize":"medium","layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:separator {"style":{"spacing":{"margin":{"top":"10px","bottom":"10px"}}}} -->
