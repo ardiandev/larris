564588b8bc0b109754a47a6390c4de83ec173f38
Ardian Pradana <ardianysp18@gmail.com>
2025-05-03 17:40:32 +0700

    Edit stories pattern

--- patterns/page-stories.php
+++ patterns/page-stories.php
@@ -7,14 +7,17 @@
 ?>
 
 <!-- wp:pattern {"slug":"larris/header"} /-->
- 
+
 <!-- wp:group {"style":{"dimensions":{"minHeight":"65vh"},"spacing":{"blockGap":"var:preset|spacing|30","margin":{"top":"30px","bottom":"30px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="min-height:65vh;margin-top:30px;margin-bottom:30px"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<h2 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Stories</h2>
-<!-- /wp:heading -->
-<!-- wp:paragraph -->
-<p>Fill out the form below, and I’ll get back to you as soon as possible. If you’re looking for a more timely response, feel free to reach out on social.</p>
-<!-- /wp:paragraph --></div>
+<div class="wp-block-group" style="min-height:65vh;margin-top:30px;margin-bottom:30px">
+  <!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+  <h2 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Stories</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:paragraph -->
+  <p>This page is a collection of my short stories and personal reflections — moments from daily life, ideas, and creative thoughts I’ve decided to share.</p>
+  <!-- /wp:paragraph -->
+</div>
 <!-- /wp:group -->
 
 <!-- wp:pattern {"slug":"larris/footer"} /-->
