29772ffda1839aec67c0ba06fb99daaa7b8fc00d
Ardian Pradana <ardianysp18@gmail.com>
2025-10-26 19:50:57 +0700

    Style hover effect to the post list in the frontpage and add link to the repo theme at the footer template part

--- patterns/portfolio-page.php
+++ patterns/portfolio-page.php
@@ -13,8 +13,8 @@
 
 <!-- wp:template-part {"slug":"header","theme":"larris","tagName":"header"} /-->
 
-<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"80vh"},"spacing":{"padding":{"right":"20px","left":"20px","top":"20px","bottom":"20px"}}},"fontSize":"medium","layout":{"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull has-medium-font-size" style="min-height:80vh;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:group {"metadata":{"name":"Hero"},"style":{"spacing":{"padding":{"right":"0px","left":"0px"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"80vh"},"spacing":{"padding":{"right":"20px","left":"20px","top":"20px","bottom":"20px"},"margin":{"bottom":"50px"}}},"fontSize":"medium","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull has-medium-font-size" style="min-height:80vh;margin-bottom:50px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:group {"metadata":{"name":"Hero"},"style":{"spacing":{"padding":{"right":"0px","left":"0px"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-right:0px;padding-left:0px"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"70px"},"blockGap":{"left":"20px"}}}} -->
 <div class="wp-block-columns" style="padding-top:70px"><!-- wp:column {"width":"80%"} -->
 <div class="wp-block-column" style="flex-basis:80%"><!-- wp:heading {"style":{"typography":{"fontSize":"24px"},"spacing":{"margin":{"bottom":"10px"}}}} -->
@@ -22,7 +22,7 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","lineHeight":1.6}}} -->
-<p style="font-size:16px;line-height:1.6">I’m Nick—a Developer Relations Advocate at Automattic, WordPress Core contributor, and hobby web developer. This site’s dedicated to my current WordPress projects and explorations into related technologies. Have a look around.</p>
+<p style="font-size:16px;line-height:1.6">Hey, I’m Ardian — a WordPress theme and plugin developer who loves building for the web. This site is where I share my latest projects and experiments. Dive in and see what I’ve been creating.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
@@ -43,31 +43,27 @@
 <p style="font-size:16px;line-height:1.6">Everything from WordPress tutorials and AI experiments to web development resources and personal updates.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[],"format":[]}} -->
-<div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|50"}}}} -->
-<!-- wp:group {"className":"post-container","style":{"border":{"radius":"10px","width":"1px"},"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"dimensions":{"minHeight":"150px"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group post-container" style="border-width:1px;border-radius:10px;min-height:150px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"18px"}}} /-->
+<!-- wp:query {"queryId":0,"query":{"perPage":5,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"ignore","inherit":false,"taxQuery":null,"parents":[],"format":[]}} -->
+<div class="wp-block-query"><!-- wp:post-template {"className":"post-has-hover","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<!-- wp:group {"className":"post-container","style":{"border":{"radius":"10px","width":"1px"},"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}},"dimensions":{"minHeight":"150px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group post-container" style="border-width:1px;border-radius:10px;min-height:150px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"category","style":{"typography":{"fontSize":"15px"}}} /-->
 
-<!-- wp:post-date {"style":{"typography":{"fontSize":"16px"}}} /-->
-
-<!-- wp:post-excerpt {"excerptLength":23,"style":{"typography":{"fontSize":"16px"}}} /--></div>
+<!-- wp:post-date {"format":"M j","style":{"typography":{"fontSize":"15px"}}} /--></div>
 <!-- /wp:group -->
-<!-- /wp:post-template -->
 
-<!-- wp:query-pagination -->
-<!-- wp:query-pagination-previous /-->
+<!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"18px"}}} /-->
 
-<!-- wp:query-pagination-numbers /-->
+<!-- wp:post-excerpt {"excerptLength":43,"className":"larris-post-excerpt","fontSize":"small"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
 
-<!-- wp:query-pagination-next /-->
-<!-- /wp:query-pagination -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p></p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
-<!-- /wp:query --></div>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right"}} -->
+<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px"><!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right"><a href="<?php echo esc_url( home_url('/blog/') ); ?>">View all →</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
